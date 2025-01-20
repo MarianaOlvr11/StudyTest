291b54b832c59b96241d38bd4e56e0b57a279030
Mariana Vitória <marxvitoria@hotmail.com>
2025-01-20 18:42:10 -0300

    Password created; Repositories created

---
+++ src/main/java/com/example/studyy/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.studyy.repository;
+
+import com.example.studyy.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}

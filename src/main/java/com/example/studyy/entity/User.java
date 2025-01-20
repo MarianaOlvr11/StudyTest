40d3940e3abbc4381b631741b5e92ad6c568c762
Mariana Vitória <marxvitoria@hotmail.com>
2025-01-20 18:27:24 -0300

    User created

---
+++ src/main/java/com/example/studyy/entity/User.java
@@ -0,0 +1,70 @@
+package com.example.studyy.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false) // coluna não nula sem nomes repetidos
+    private String username;
+
+    @Column(nullable = false) // coluna não nula
+    private String password;
+
+    @Column(nullable = false, unique = true) // coluna não nula sem emails repetidos
+    private String email;
+
+    // Construtores
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    // Getters e Setters
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    // toString, equals, hashCode (opcional, mas útil para depuração)
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+}

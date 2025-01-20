0df537cdc278d944c04214a1b5a875c6cc739606
Mariana Vitória <marxvitoria@hotmail.com>
2025-01-20 18:11:23 -0300

    Server Config

---
+++ src/main/java/com/example/studyy/StudyyApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyy/StudyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
